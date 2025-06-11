df7c6e5166bac99bc1ac6859f5f8806cbf626349
shindelr <129821483+shindelr@users.noreply.github.com>
2025-06-10 18:47:58 -0700

    Added electron boilerplate

---
+++ electron/main.js
@@ -0,0 +1,32 @@
+const { create } = require('domain');
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow(){
+    const win = new BrowserWindow({
+        width: 1200,
+        height: 800,
+        webPreferences: {
+            contextIsolation: true,
+        },
+    });
+
+    // Load vite, dev or production?
+    const isDev = !app.isPackaged;
+    if (isDev){
+        win.loadURL('http://localhost:5173');
+    }
+    else {
+        win.loadFile(path.join(__dirname, '../dist/index.html'));
+    }
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') app.quit();
+});
+
+app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+});
